5468ca00733053906188a4d5fc249d658be11ffe
Marwan4373 <marwanhassan4373@gmail.com>
2024-07-04 23:31:12 +0300

    first add

---
+++ people.js
@@ -0,0 +1,7 @@
+const people = ["marwan", "osama", "omar", "mostafa"];
+const ages = [28, 29, 26, 29]
+console.log(people);
+
+module.exports = {
+     people, ages
+}
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+setTimeout(()=> {
+// console.log("in the setTimeout");
+// clearInterval(int)
+// }, 5000);
+
+// const int = setInterval(() => {
+// console.log("in the interval")
+// }, 1000);
